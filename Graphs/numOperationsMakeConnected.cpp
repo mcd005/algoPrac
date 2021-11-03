0a17c27f859c5e8e857793b59e860b41b4e5927d
mcd005 <j.pmcdonald005@gmail.com>
2021-11-03 22:46:47 +0000

    WIP need to reach steady state

--- Graphs/numOperationsMakeConnected.cpp
+++ Graphs/numOperationsMakeConnected.cpp
@@ -5,25 +5,44 @@ public:
     {
         if (connections.size() < n - 1) return -1;
 
-        std::unordered_map<int, int> pcToWhichNetwork;
-        int networkNumber = 0
+        pcToWhichNetwork.resize(n);
+        std::iota(pcToWhichNetwork.begin(), pcToWhichNetwork.end(), 0);
+
         for (auto& conn : connections)
         {
-            if (pcToWhichNetwork.count(conn[0]) > 0)
-            {
-                pcToWhichNetwork[conn[1]] = pcToWhichNetwork[conn[0]];
-            }
-            else if (pcToWhichNetwork.count(conn[1]) > 0)
-            {
-                pcToWhichNetwork[conn[0]] = pcToWhichNetwork[conn[1]];
-            }
-            else
-            {
-                network
-            }
+            _union(conn[0], conn[1]);
         }
+
+        return std::set<int>(pcToWhichNetwork.begin(), pcToWhichNetwork.end()).size() - 1;
+    }
+private:
+    std::vector<int> pcToWhichNetwork;
+
+    void _union(int x, int y)
+    {
+        pcToWhichNetwork[find(y)] = find(x);
+    }
+
+    int find(int a)
+    {
+        if (pcToWhichNetwork[a] != a)
+        {
+            return find(pcToWhichNetwork[a]);
+        }
+        return a;
     }
 };
+
+// n = 4, connections = [[0,1],[0,2],[1,2]]
+// parents  = [0, 1, 2, 3]
+// parents  = [0, 0, 0, 3]
+
+// Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+// parents  = [0, 1, 2, 3, 4, 5]
+// parents  = [0, 0, 0, 3, 4, 5]
+
+// Input: n = 5, connections = [[0,1],[0,2],[3,4],[2,3]]
+// parents  = [0, 0, 0, 3, 0]
 
 /*
 Check that there are n - 1 cables
