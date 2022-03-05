1596af5fb6acb9fd5b32792f4074c9c6fbf8b379
mcd005 <j.pmcdonald005@gmail.com>
2022-03-05 22:14:23 +0000

    Tidy generated file with sed

--- copy_leetcode_question.sh
+++ copy_leetcode_question.sh
@@ -1,8 +1,12 @@
 set -eu
 
 QUESTION_NO=$1 
+GENERATED_FILE=~/.leetcode/${QUESTION_NO}\.*
+echo ${GENERATED_FILE}
 
 NEW_FILENAME=$(cd ~/.leetcode/ && ls ${QUESTION_NO}\.* | sed -e "s/^[1-9][0-9]*\.//" -e "s/\-/\_/g")
 echo $NEW_FILENAME
 
-cp -n ~/.leetcode/${QUESTION_NO}\.*  ./${NEW_FILENAME}
+sed -i '/^#/d;/^$/d' ${GENERATED_FILE}
+
+cp -n ${GENERATED_FILE} ./${NEW_FILENAME}
