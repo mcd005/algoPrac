f0822226a0500a5a77d5fc68f988af19af3f6f56
mcd005 <j.pmcdonald005@gmail.com>
2022-03-09 21:32:38 +0000

    Updating for LC

--- Maths Problems/FizzBuzz.py
+++ Maths Problems/FizzBuzz.py
@@ -1,22 +1,29 @@
+# https://leetcode.com/problems/fizz-buzz/
+# Version 2 - List comprehension
+# Time complexity     O(n)
+# Space complexity    O(1)
+def fizzBuzz(self, n):
+    return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+
+# Version 1 - For loop, build string
+# Time complexity     O(n)
+# Space complexity    O(1)
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+        for i in range(1, n + 1):
+            current_ = ""
+            if i % 3 == 0:
+                result += "Fizz"
+            if i % 5 == 0:
+                result += "Buzz"
+            if not result:
+                result = str(i)
+            output.append(result)
+        return output
 '''
 Classic algo problem
 
 Make sure you avoid mistakes with bad if else statements
 Account for the multiples of 15 edge cases
-
-Time compelxity     O(n)
-Space complexity    O(1)
 '''
-
-def fizzBuzz(N):
-    for i in range(1, N + 1):
-        result = ""
-        if i % 3 == 0:
-            result += "Fizz"
-        if i % 5 == 0:
-            result += "Buzz"
-        if not result:
-            result = i
-        print(result)
-
-fizzBuzz(100)
